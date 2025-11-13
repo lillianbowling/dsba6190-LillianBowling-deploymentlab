e2a3ceecfa45a59c11851815a6760753800e50b1
lillianbowling <lillybowling17@gmail.com>
2025-11-12 19:21:38 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "cford38"
+  default     = "lilb"
 }
 
 variable "class_name" {
